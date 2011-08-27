328e4d4646feb565068e8dda00c1b0cfbcc95c20
Jason Axelson <jason.axelson@gmail.com>
2011-08-27 08:28:00 -1000

    Working on tipster
    http://whyandroid.com/android/227-tipster-building-a-tip-calculator-for-the-android-os.html

---
+++ Tipster/src/com/jaxelson/TipsterActivity.java
@@ -0,0 +1,13 @@
+package com.jaxelson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TipsterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
